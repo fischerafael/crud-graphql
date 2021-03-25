f2d9c00ce9df1e4b5cc9e9c644b5f94461976ee5
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-03-25 00:00:55 -0300

    removed parking

--- src/app/graphql/resolvers/index.js
+++ src/app/graphql/resolvers/index.js
@@ -1,4 +1,3 @@
-const Parking = require("../../database/Models/Parking");
 const { userService } = require("../../services/user");
 
 const resolvers = {
@@ -10,21 +9,6 @@ const resolvers = {
     createUser: async (_, { email }) => await userService.create(email),
     updateUser: async (_, { id, email }) => await userService.update(id, email),
     deleteUser: async (_, { id }) => await userService.delete(id),
-    createParking: async (_, { price, name, whats, lat, long, owner }) => {
-      const location = {
-        coordinates: [-122.5, 37.7],
-      };
-      console.log(location);
-      const parking = await Parking.create({
-        price,
-        name,
-        whats,
-        owner,
-        location: location,
-      });
-
-      return parking;
-    },
   },
 };
 
--- src/app/graphql/typeDefs/index.js
+++ src/app/graphql/typeDefs/index.js
@@ -5,32 +5,16 @@ const typeDefs = gql`
     id: ID!
     email: String!
   }
-  type Parking {
-    id: ID
-    price: Float!
-    name: String!
-    whats: Int!
-    lat: Float!
-    long: Float!
-    owner: String!
-  }
 
   type Query {
     users: [User!]!
     user(id: ID!): User
   }
+
   type Mutation {
     createUser(email: String!): User!
     updateUser(id: ID!, email: String): User!
     deleteUser(id: ID!): User!
-    createParking(
-      price: Float!
-      name: String!
-      whats: Int!
-      lat: Float!
-      long: Float!
-      owner: ID!
-    ): Parking!
   }
 `;
 
